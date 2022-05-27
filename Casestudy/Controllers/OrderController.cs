31eecbd66546df9dd95369d511eeaff417ba6052
Soohwan123 <90574797+Soohwan123@users.noreply.github.com>
2022-05-27 10:29:32 -0400

    Items order feature

---
+++ Casestudy/Controllers/OrderController.cs
@@ -0,0 +1,41 @@
+﻿using Casestudy.DAL;
+using Casestudy.DAL.DAO;
+using Casestudy.DAL.DomainClasses;
+using Microsoft.AspNetCore.Mvc;
+using Casestudy.Helpers;
+using Microsoft.AspNetCore.Authorization;
+namespace Casestudy.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        readonly AppDbContext _ctx;
+        public OrderController(AppDbContext context) // injected here
+        {
+            _ctx = context;
+        }
+        [HttpPost]
+        [Produces("application/json")]
+        public async Task<ActionResult<string>> Index(OrderHelper helper)
+        {
+            string retVal;
+            try
+            {
+                CustomerDAO uDao = new(_ctx);
+                Customer? orderOwner = await uDao.GetByEmail(helper.Email);
+                OrderDAO oDao = new(_ctx);
+                int orderId = await oDao.AddOrder(orderOwner!.Id, helper.Selections!);
+                retVal = orderId > 0
+                ? "Order " + orderId + " Created! Goods backordered!"
+                : "Order not created";
+            }
+            catch (Exception ex)
+            {
+                retVal = "Order not created " + ex.Message;
+            }
+            return retVal;
+        }
+    }
+}
---
+++ Casestudy/DAL/DAO/OrderDAO.cs
@@ -0,0 +1,76 @@
+﻿using Casestudy.DAL.DomainClasses;
+using Casestudy.Helpers;
+
+namespace Casestudy.DAL.DAO
+{
+    public class OrderDAO
+    {
+        private readonly AppDbContext _db;
+        public OrderDAO(AppDbContext ctx)
+        {
+            _db = ctx;
+        }
+        public async Task<int> AddOrder(int customerid, OrderSelectionHelper[] selections)
+        {
+            int orderId = -1;
+            // we need a transaction as multiple entities involved
+            using (var _trans = await _db.Database.BeginTransactionAsync())
+            {
+                try
+                {
+                    Order order = new();
+                    order.CustomerId = customerid;
+                    order.OrderDate = System.DateTime.Now;
+                    order.OrderAmount = 0.0M;
+                    // calculate the totals and then add the order row to the table
+                    foreach (OrderSelectionHelper selection in selections)
+                    {
+                        order.OrderAmount += selection.Item!.MSRP * selection.Qty ;
+                    }
+
+                    await _db.Orders!.AddAsync(order);
+                    await _db.SaveChangesAsync();
+                    // then add each item to the orderitems table
+                    foreach (OrderSelectionHelper selection in selections)
+                    {
+                        OrderLineItem oItem = new();
+                        oItem.Product = selection.Item!;
+                        oItem.ProductId = selection.Item!.Id;
+                        oItem.SellingPrice = selection.Item!.MSRP * (decimal)selection.Qty;
+                        oItem.OrderId = order.Id;
+                        if (selection.Qty <= oItem.Product!.QtyOnHand)
+                        {
+                            oItem.Product!.QtyOnHand -= selection.Qty;
+                            oItem.QtySold = selection.Qty;
+                            oItem.QtyBackOrdered = 0;
+                            oItem.QtyOrdered = selection.Qty;
+                        }
+                        else
+                        {
+                            oItem.Product!.QtyOnHand = 0;
+                            oItem.QtySold = oItem.Product!.QtyOnHand;
+                            oItem.QtyBackOrdered = selection.Qty - oItem.Product!.QtyOnHand;
+                            oItem.QtyOrdered = selection.Qty;
+                        }
+
+                        await _db.OrderLineItems!.AddAsync(oItem);
+                        _db.Products!.Update(oItem.Product!);
+                        await _db.SaveChangesAsync();
+                    }
+                    // test trans by uncommenting out these 3 lines
+                    //int x = 1;
+                    //int y = 0;
+                    //x = x / y;
+                    await _trans.CommitAsync();
+                    orderId = order.Id;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    await _trans.RollbackAsync();
+                }
+            }
+            return orderId;
+        }
+    }
+}
